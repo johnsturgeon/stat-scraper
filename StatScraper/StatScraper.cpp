b99d364464c6f7020dec52bff2b8a4bf825e1aff
John Sturgeon <john.sturgeon@me.com>
2023-02-27 16:43:08 -0800

    Checkpoint commit: Added SkillRank data

--- StatScraper/StatScraper.cpp
+++ StatScraper/StatScraper.cpp
@@ -94,9 +94,8 @@ void StatScraper::handleTick() {
 		onlineGame.startGame(gameWrapper);
 		sendOnlineGameToServer();
 	}
-	if (onlineGame.shouldUpdateRoster(gameWrapper)) {
-		std::vector<Player> roster = getLiveRoster();
-		onlineGame.roster = roster;
+	bool didUpdateGame = onlineGame.updateGameIfNecessary(gameWrapper);
+	if (didUpdateGame) {
 		sendOnlineGameToServer();
 	}
 }
@@ -104,55 +103,37 @@ void StatScraper::handleTick() {
 bool StatScraper::shouldRun() {
 	// Check to see if server exists
 	ServerWrapper server = gameWrapper->GetOnlineGame();
-	if (!server) { return false; }
-	GameSettingPlaylistWrapper playlist = server.GetPlaylist();
-	if (playlist.memory_address == NULL) { return false; }
-	// we only care about duos right now (playlist 11)
-	if (playlist.GetPlaylistId() != 11) { return false; }
+	if (!server) { return false; } 
+	int currentPlaylist = getCurrentPlaylistInt(server);
+	if (!playlistIsValid(currentPlaylist)) { return false; }
 	return true;
 }
 
+int StatScraper::getCurrentPlaylistInt(ServerWrapper server) {
+	GameSettingPlaylistWrapper playlist = server.GetPlaylist();
+	if (playlist.memory_address == NULL) { return -1; }
+	return playlist.GetPlaylistId();
+}
 
 void StatScraper::sendOnlineGameToServer() {
 	json jsonOnlineGame = onlineGame;
     CurlRequest req;             
     req.url = onlineGameUrl;
     req.body = jsonOnlineGame.dump();
+	LOG("Sending JSON {}", req.body);
     HttpWrapper::SendCurlJsonRequest(req, [this](int code, std::string result) {});
 }
+bool StatScraper::playlistIsValid(int idToCheck) {
+	std::vector<PlaylistIds> validPlaylists;
+	validPlaylists.push_back(PlaylistIds::RankedTeamDoubles);
 
-std::vector<Player> StatScraper::getLiveRoster() {
-	std::vector<Player> players;
-	ServerWrapper game = gameWrapper->GetOnlineGame();
-	if (!game) { return players; }
-	ArrayWrapper<PriWrapper> priList = game.GetPRIs();
-	int player_count = priList.Count();
-	MMRWrapper mmrWrapper = gameWrapper->GetMMRWrapper();
-	PlayerControllerWrapper pc = gameWrapper->GetPlayerController();
-	PriWrapper primaryPRI = pc.GetPRI();
-	int primaryPlayerID = primaryPRI.GetPlayerID();
-
-	for (int i = 0; i < player_count; i++) {
-		PriWrapper pri = priList.Get(i);
-		Player aPlayer = Player();
-		bool isPrimaryPlayer = false;
-		if (pri.GetPlayerID() == primaryPlayerID) {
-			isPrimaryPlayer = true;
+	for (PlaylistIds id : validPlaylists) {
+		int idInt = static_cast<int>(id);
+		if (idInt == idToCheck) {
+			return true;
 		}
-		aPlayer.name = pri.GetPlayerName().ToString();
-		aPlayer.bakkes_player_id = pri.GetPlayerID();
-		aPlayer.platform_id_string = pri.GetUniqueIdWrapper().GetIdString();
-		aPlayer.team_num = pri.GetTeamNum();
-		aPlayer.score = pri.GetMatchScore();
-		aPlayer.goals = pri.GetMatchGoals();
-		aPlayer.saves = pri.GetMatchSaves();
-		aPlayer.assists = pri.GetMatchAssists();
-		aPlayer.shots = pri.GetMatchShots();
-		aPlayer.mmr = mmrWrapper.GetPlayerMMR(pri.GetUniqueIdWrapper(), 11);
-		aPlayer.is_primary_player = isPrimaryPlayer;
-		players.push_back(aPlayer);
 	}
-	return players;
+	return false;
 }
 
 
@@ -211,73 +192,148 @@ void OnlineGame::startGame(std::shared_ptr<GameWrapper> gameWrapper) {
 	match_id = game.GetMatchGUID();
 	start_timestamp = getUnixTimestamp();
 	primary_player_starting_mmr = getPlayerMMR(gameWrapper);
-	primary_player_ending_mmr = primary_player_starting_mmr;
 	PlayerControllerWrapper pc = gameWrapper->GetPlayerController();
 	PriWrapper pri = pc.GetPRI();
 	primary_bakkes_player_id = pri.GetPlayerID();
+	playlist_id = StatScraper::getCurrentPlaylistInt(game);
 }
 
 void OnlineGame::endGame(std::shared_ptr<GameWrapper> gameWrapper) {
-	end_timestamp = getUnixTimestamp();
-	for (Player p : roster) {
-		if (p.is_primary_player) {
-			primary_player_ending_mmr = p.mmr;
-		}
-	}
 	game_state = GAME_ENDED;
 }
 
 int OnlineGame::getRosterTotalScore() {
 	int totalScore = 0;
-	for (Player p : roster) {
-		totalScore += p.score;
+	for (auto const& [key, val] : rosterMap) {
+		if (val.is_in_game) {
+			totalScore += val.score;
+		}
 	}
 	return totalScore;
 }
 
-bool OnlineGame::shouldUpdateRoster(std::shared_ptr<GameWrapper> gameWrapper) {
+int OnlineGame::getRosterCount() {
+	int count = 0;
+	for (auto const& [key, val] : rosterMap) {
+		if (val.is_in_game) {
+			count += 1;
+		}
+	}
+	return count;
+}
+
+bool OnlineGame::updateGameIfNecessary(std::shared_ptr<GameWrapper> gameWrapper) {
 	ServerWrapper game = gameWrapper->GetOnlineGame();
 	if (!game) { return false; }
+
+	// first let's check to see if it's a 'roster' update
 	ArrayWrapper<PriWrapper> priList = game.GetPRIs();
-	int liveTotalScore = 0;
 	int livePlayerCount = priList.Count();
-	float livePrimaryPlayerMMR = getPlayerMMR(gameWrapper);
+	if (livePlayerCount != getRosterCount()) {
+		updateRoster(gameWrapper);
+		return true;
+	}
+	
+	// now let's see if the score changed
+	int liveTotalScore = 0;
 	for (int i = 0; i < priList.Count(); i++) {
 		PriWrapper pri = priList.Get(i);
 		liveTotalScore += pri.GetMatchScore();
 	}
-	if (liveTotalScore != getRosterTotalScore() or
-		livePlayerCount != roster.size()) {
+	if (liveTotalScore != getRosterTotalScore()) {
+		updateRoster(gameWrapper);
 		return true;
 	}
-	if (!floatsAlmostEqual(livePrimaryPlayerMMR, primary_player_ending_mmr)) {
-		primary_player_ending_mmr = livePrimaryPlayerMMR;
+
+	// now check to see if mmr has changed
+	float livePrimaryPlayerMMR = getPlayerMMR(gameWrapper);
+	if (!floatsAlmostEqual(livePrimaryPlayerMMR, primary_player_ending_mmr())) {
+		updateRoster(gameWrapper);
 		return true;
 	}
 	return false;
 }
 
 void OnlineGame::resetGame() {
-	roster.clear();
+	rosterMap.clear();
 	match_id = ""; 
 	start_timestamp = 0;
-	end_timestamp = 0;
+	last_update_timestamp = 0;
 	primary_player_starting_mmr = 0.0;
-	primary_player_ending_mmr = 0.0;
 	primary_bakkes_player_id = 0;
 	game_state = NO_GAME;
 }
 
 float OnlineGame::getPlayerMMR(std::shared_ptr<GameWrapper> gameWrapper) {
-	MMRWrapper mmrWrapper = gameWrapper->GetMMRWrapper();
+	MMRWrapper mmrWrapper = gameWrapper->GetMMRWrapper();  
 	PlayerControllerWrapper pc = gameWrapper->GetPlayerController();
 	PriWrapper pri = pc.GetPRI();
 	if (!pc) { return 0.0; }
 	if (!pri) { return 0.0; }
-	return mmrWrapper.GetPlayerMMR(pri.GetUniqueIdWrapper(), DUALS_PLAYLIST_ID);
+	return mmrWrapper.GetPlayerMMR(pri.GetUniqueIdWrapper(), static_cast<int>(PlaylistIds::RankedTeamDoubles));
 }
 
+void OnlineGame::updateRoster(std::shared_ptr<GameWrapper> gameWrapper) {
+	// first let's set the player's 'in_game' to false for all the players
+	for (auto const& [key, val] : rosterMap) {
+		rosterMap[key].is_in_game = false;
+	}
+	this->last_update_timestamp = getUnixTimestamp();
+	ServerWrapper game = gameWrapper->GetOnlineGame();
+	if (!game) { return; }
+	ArrayWrapper<PriWrapper> priList = game.GetPRIs();
+	int player_count = priList.Count();
+	MMRWrapper mmrWrapper = gameWrapper->GetMMRWrapper();
+	PlayerControllerWrapper pc = gameWrapper->GetPlayerController();
+	PriWrapper primaryPRI = pc.GetPRI();
+	int primaryPlayerID = primaryPRI.GetPlayerID();
+	for (int i = 0; i < player_count; i++) {
+		PriWrapper pri = priList.Get(i);
+		Player aPlayer = Player();
+		bool isPrimaryPlayer = false;
+		std::string platform_id_string = pri.GetUniqueIdWrapper().GetIdString();
+		if (pri.GetPlayerID() == primaryPlayerID) {
+			isPrimaryPlayer = true;
+		}
+		aPlayer.name = pri.GetPlayerName().ToString();
+		aPlayer.bakkes_player_id = pri.GetPlayerID();
+		aPlayer.platform_id_string = platform_id_string;
+		aPlayer.team_num = pri.GetTeamNum();
+		aPlayer.score = pri.GetMatchScore();
+		aPlayer.goals = pri.GetMatchGoals();
+		aPlayer.saves = pri.GetMatchSaves();
+		aPlayer.assists = pri.GetMatchAssists();
+		aPlayer.shots = pri.GetMatchShots();
+		aPlayer.mmr = mmrWrapper.GetPlayerMMR(pri.GetUniqueIdWrapper(), static_cast<int>(PlaylistIds::RankedTeamDoubles));
+		aPlayer.skill_rank = mmrWrapper.GetPlayerRank(pri.GetUniqueIdWrapper(), static_cast<int>(PlaylistIds::RankedTeamDoubles));
+		aPlayer.is_primary_player = isPrimaryPlayer;
+		aPlayer.is_in_game = true;
+		rosterMap[platform_id_string] = aPlayer;
+	}
+}
 
-void OnlineGame::updateRoster(std::vector<Player> roster) {
+// properties
+std::vector<Player> OnlineGame::roster() const {
+	std::vector<Player> rosterVector;
+	for (auto const& [key, val] : rosterMap) {
+		rosterVector.push_back(val);
+	}
+	return rosterVector;
+}
 
+int OnlineGame::end_timestamp() const {
+	return getUnixTimestamp();
+}
+
+float OnlineGame::primary_player_ending_mmr() const {
+	return primary_player().mmr;
+}
+
+Player OnlineGame::primary_player() const {
+	for (auto const& [key, val] : rosterMap) {
+		if (val.is_primary_player) {
+			return val;
+		}
+	}
+	return Player();
 }
--- StatScraper/StatScraper.h
+++ StatScraper/StatScraper.h
@@ -7,8 +7,6 @@
 #include "json.hpp"
 #include "version.h"
 
-const int DUALS_PLAYLIST_ID = 11;
-
 using json = nlohmann::json;
 
 enum GameState {
@@ -29,38 +27,77 @@ public:
 	int assists = 0;
 	int shots = 0;
 	float mmr = 0.0;
+	SkillRank skill_rank = { 0,0,0 };
 	bool is_primary_player = false;
+	bool is_in_game = false;
 };
 
-NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(
-	Player, name, bakkes_player_id, platform_id_string, team_num,
-	score, goals, saves, assists, shots, mmr, is_primary_player)
+void to_json(json& j, const Player& p) {
+	j = json {
+		{"name", p.name},
+		{"bakkes_player_id", p.bakkes_player_id},
+		{"platform_id_string", p.platform_id_string},
+		{"team_num", p.team_num},
+		{"is_in_game", p.is_in_game},
+		{"score", p.score},
+		{"goals", p.goals},
+		{"saves", p.saves},
+		{"assists", p.assists},
+		{"shots", p.shots},
+		{"mmr", p.mmr},
+		{"is_primary_player", p.is_primary_player},
+		{"skill_rank", {
+			{"tier", p.skill_rank.Tier},
+			{"division", p.skill_rank.Division},
+			{"matches_played", p.skill_rank.MatchesPlayed}
+			}
+		}
+	};
+}
+
 
 class OnlineGame {
 private:
 	float getPlayerMMR(std::shared_ptr<GameWrapper>);
 	int getRosterTotalScore();
+	int getRosterCount();
+	void updateRoster(std::shared_ptr<GameWrapper> gameWrapper);
+	std::map<std::string, Player> rosterMap;
+	Player primary_player() const;
 
 public:
 	void startGame(std::shared_ptr<GameWrapper>);
 	void endGame(std::shared_ptr<GameWrapper>);
-	bool shouldUpdateRoster(std::shared_ptr<GameWrapper> gameWrapper);
-	void updateRoster(std::vector<Player> roster);
+	bool updateGameIfNecessary(std::shared_ptr<GameWrapper>);
 	void resetGame();
 	std::string match_id;
 	int start_timestamp = 0;
-	int end_timestamp = 0;
-	std::vector<Player> roster;
+	int playlist_id = 0;
+	int last_update_timestamp = 0;
 	float primary_player_starting_mmr = 0.0;
-	float primary_player_ending_mmr = 0.0;
 	int primary_bakkes_player_id = 0;
 	GameState game_state = NO_GAME;
+
+	int end_timestamp() const;
+	float primary_player_ending_mmr() const;
+	std::vector<Player> roster() const;
+
 };
 
-NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(
-	OnlineGame, match_id, start_timestamp, end_timestamp, roster, primary_player_starting_mmr,
-	primary_player_ending_mmr, primary_bakkes_player_id, game_state)
-
+void to_json(json& j, const OnlineGame& g) {
+	j = json{
+		{"match_id", g.match_id},
+		{"start_timestamp", g.start_timestamp},
+		{"end_timestamp", g.end_timestamp()},
+		{"last_update_timestamp", g.last_update_timestamp},
+		{"roster", g.roster()},
+		{"primary_player_starting_mmr", g.primary_player_starting_mmr},
+		{"primary_player_ending_mmr", g.primary_player_ending_mmr()},
+		{"primary_bakkes_player_id", g.primary_bakkes_player_id},
+		{"game_state", g.game_state},
+		{"playlist_id", g.playlist_id}
+	};
+}
 
 struct StatEventParams {
 	// always primary player
@@ -81,9 +118,8 @@ class StatScraper: public BakkesMod::Plugin::BakkesModPlugin
 	void sendLog(std::string);
 	bool shouldRun();
 	void handleTick();
-	std::vector<Player> getLiveRoster();
 	void sendOnlineGameToServer();
-
+	bool playlistIsValid(int);
 	/*
 	TODO Figure out what's used and not
 	*/
@@ -96,6 +132,7 @@ class StatScraper: public BakkesMod::Plugin::BakkesModPlugin
 public:
 	//void RenderSettings() override; // Uncomment if you wanna render your own tab in the settings menu
 	//void RenderWindow() override; // Uncomment if you want to render your own plugin window
+	static int  getCurrentPlaylistInt(ServerWrapper);
 
 private:
 
